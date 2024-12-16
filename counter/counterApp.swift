8bb42a39758d977bcf17938403641ac303740e21
aBirrueta <alejandrobirru3ta@gmail.com>
2024-12-16 11:02:57 -0800

    Initial Commit

---
+++ counter/counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterApp.swift
+//  counter
+//
+//  Created by Alejandro Birrueta on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ counterTests/counterTests.swift
@@ -0,0 +1,17 @@
+//
+//  counterTests.swift
+//  counterTests
+//
+//  Created by Alejandro Birrueta on 12/16/24.
+//
+
+import Testing
+@testable import counter
+
+struct counterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
